8408a723fcfefd5826e8db22aef8942486df61dd
MiroslavRosenov <miroslav.rosenov39@gmail.com>
2022-03-18 02:11:11 +0200

    Updated some stuff

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from quart import Quart
+from quart import Quart, render_template, request
 
 
 app = Quart(__name__)
 
 @app.route('/')
 async def home():
-    return "Hello World"
+    return await render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+async def login():
+    if request.method == 'POST':
+        form = await request.form
+        return form
+    else:
+        return "👀"
 
 if __name__ == '__main__':
     app.run(debug=True)
